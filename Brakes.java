5813bfc9279828ea66855a87bb963ec43abae431
isaac <isaac@rubenfamily.com>
2020-10-29 18:27:19 -0700

    let breaks cool off
    also adds a toString for breaks cause easy

--- Brakes.java
+++ Brakes.java
@@ -1,12 +1,12 @@
 public class Brakes {
     private boolean works = true; //i sure hope it does
     private int temp = 72; //f
-    private int wearLevel = 0; //1 to 10
+    private int wearLevel = 0; //0 to 10
     private double breakingTime = 0; //cumulative
 
     //prints general break health info
     public void breakHealth() {
-        System.out.println("ntempature: "+temp+"\ntime spent breaking: "+breakingTime+"\nhealth:"+wearLevel+"\ncan break? "+works);
+        System.out.println("tempature: "+temp+"\ntime spent breaking: "+breakingTime+"\nwear level:"+wearLevel+"\ncan break? "+works);
     }
     //temp getter
     public int getTemp() {
@@ -22,7 +22,20 @@ public class Brakes {
         if(
             ((int) Math.random() * time) > 15 || time > 40 //this is not a good way to do this
         ){wearLevel += 1;}
+        System.out.println("Wear level is now: "+wearLevel);
         return Math.max(1,time/3);
+    }
+
+    //lets breaks cool down
+    public void unusedFor(int time){
+        temp = Math.max(72,time/20);
+        System.out.println("Break temp is now: "+temp);
+    }
+
+    @Override
+    public String toString(){
+        return
+        "tempature: "+temp+"\ntime spent breaking: "+breakingTime+"\nwear level:"+wearLevel+"\ncan break? "+works;
     }
 
 }
