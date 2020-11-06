a37b59442edfb5f56aa774281a801f84e9293dee
maddie <maddie.behr2007@gmail.com>
2020-11-05 18:19:21 -0800

    Finished with Wheels

--- Wheels.java
+++ Wheels.java
@@ -13,11 +13,10 @@ public class Wheels {
         this.speed = speed;
         howMPH = speed;
         if (shape.equals(array[1])){
-            System.out.println("This will be a smooth ride");
            howMPH = howMPH + 5; 
 
         }else{
-            System.out.println("That will be a bumpy ride") ;
+
             howMPH = howMPH - 3; 
         }
 
@@ -25,29 +24,57 @@ public class Wheels {
 
         if(size >= 15){
             howMPH = howMPH - 10;
-            System.out.println("This bike is too big;it will not move very fast") ;
-        }else if (size < 15 && 6 > size ){
+            
+        }else if (size < 15 && 6 >= size ){
             howMPH = howMPH + 5;
-            System.out.println("This is a good sized bike") ;
+           
         }else if( 5>= size){
             howMPH = howMPH - 5;
-            System.out.println("That is an extreamly small bike that won't move very fast") ;
+            
         }//end of is 
 
         //end of size
-        
         if((airPressure >= 130)){
             howMPH = 0;
-            System.out.println("You can't ride a bike with busted wheels") ;
-        }else if(60<airPressure && airPressure< 129){
+            
+        }else if(60<=airPressure && airPressure<= 129){
             howMPH =howMPH+5;
-            System.out.println("Woooo, These tires are useable") ;
-        }else if (airPressure <59){
+            
+        }else if (airPressure <=59){
             howMPH =howMPH-5;
-            System.out.println("These are flat tires") ;
+            
         }// end of if 
-
-        //end of air pressure
- 
     }
+        public String toString() {
+            String output = new String();
+            String output2 = new String();
+            String output3 = new String();
+            if (shape.equals(array[1])){
+               output = output + shape + (" : This will be a smooth ride ");
+            }else{
+                output = output + shape + (" : That will be a bumpy ride ") ;
+            }
+    
+            //end of shape
+            if(size >= 15){
+                output2 = output2 +size + (" : This bike is too big; it will not move very fast ") ;
+            }else if (size < 15 || 6 >= size ){
+                output2 = output2 + size +(" :  This is a good sized bike ") ;
+            }else if( 5>= size){
+                output2 = output2 + size + ( " : That is an extreamly small bike that won't move very fast  " ) ;
+            }//end of is 
+    
+            //end of size
+            
+            if((airPressure >= 130)){
+                output3  = output3  + airPressure + (" :  You can't ride a bike with busted wheels ") ;
+            }else if(60<=airPressure && airPressure<= 129){
+                output3  = output3  + airPressure+ (" : Woooo, These tires are useable ") ;
+            }else if (airPressure <59){
+                output3  = output3  + airPressure + (" : These are flat tires ") ;
+            }// end of if 
+            return output + output2 + output3 +"This Bike will go " + howMPH + " miles per hour ";
+          }//end tostring
+        //end of air pressure
+    
 }
